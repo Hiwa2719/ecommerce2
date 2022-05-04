ba08fd2c55dc98cd9b66db8602dca0262acc3a23
hiwa2719 <hiahmadyan@gmail.com>
2022-05-04 18:48:41 +0430

    adding header and footer

--- frontent/src/App.js
+++ frontent/src/App.js
@@ -1,7 +1,17 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
     return (
         <div>
-
+            <Header/>
+            <main className="py-3">
+                <div className="container">
+                    <h1>Welcome</h1>
+                </div>
+            </main>
+            <Footer/>
         </div>
     );
 }
---
+++ frontent/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <div className="row">
+                    <div className="col py-3 text-center">
+                        Copyright &copy; Hiwa
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
