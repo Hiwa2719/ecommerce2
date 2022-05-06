6ac9d730f7311d9930407da7bc72a8017d6f25cb
hiwa2719 <hiahmadyan@gmail.com>
2022-05-06 12:50:36 +0430

    adding product view and DRF

--- base/urls.py
+++ base/urls.py
@@ -8,5 +8,5 @@ app_name = 'base'
 urlpatterns = [
     path('', views.get_routes, name='routes'),
     path('products/', views.get_products, name='get_products'),
-
+    path('products/<int:pk>/', views.get_product, name='get_product'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .products import products
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view()
 def get_routes(request):
-    return JsonResponse('hello', safe=False)
+    return Response('hello')
 
 
+@api_view()
 def get_products(request):
-    return JsonResponse(products, safe=False)
+    return Response(products)
 
+
+@api_view()
+def get_product(request, pk):
+    product = None
+    for item in products:
+        if item['_id'] == str(pk):
+            product = item
+            break
+    return Response(product)
