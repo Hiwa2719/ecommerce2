537507c9ee8241ce5dd35d4ee77634adb903e00b
hiwa2719 <hiahmadyan@gmail.com>
2022-05-06 15:50:35 +0430

    adding Product model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+User = get_user_model()
+
+
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    brand = models.CharField(max_length=200, blank=True, null=True)
+    category = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    rating = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    numReviews = models.IntegerField(blank=True, null=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    countInStock = models.IntegerField(blank=True, null=True, default=0)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def get_absolute_url(self):
+        return reverse('base:get_product', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.name or ''
