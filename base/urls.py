f5dbe8f555128cda5ef2f99c16cc973f6695d760
unknown <hiahmadyan@gmail.com>
2022-05-22 22:19:19 +0430

    changing JWT view and serializer

--- base/urls.py
+++ base/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 app_name = 'base'
 
 
 urlpatterns = [
-    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.get_routes, name='routes'),
     path('products/', views.get_products, name='get_products'),
     path('products/<int:pk>/', views.get_product, name='get_product'),
