62ba245549935268357bea1f2f8ff216ec22bf2e
unknown <hiahmadyan@gmail.com>
2022-05-24 17:00:21 +0430

    addin user Actions and reducers

--- base/urls.py
+++ base/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path, include
+
 from . import views
 
 app_name = 'base'
 
+product_urls = [
+    path('', views.get_products, name='get_products'),
+    path('<int:pk>/', views.get_product, name='get_product'),
+]
 
+users_urls = [
+    path('', views.get_users, name='users'),
+    path('register/', views.register_user, name='user-register'),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('profile/', views.get_user_profile, name='user-profile')
+]
 urlpatterns = [
-    path('products/', views.get_products, name='get_products'),
-    path('products/<int:pk>/', views.get_product, name='get_product'),
-    path('users/', include([
-        path('', views.get_users, name='users'),
-        path('register/', views.register_user, name='user-register'),
-        path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-        path('profile/', views.get_user_profile, name='user-profile')
-    ]))
+    path('products/', include(product_urls)),
+    path('users/', include(users_urls))
 ]
