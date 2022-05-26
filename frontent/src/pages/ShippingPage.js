ee8a692c4baeb7f84d4e2a6b2164ccf8d1aaaefe
unknown <hiahmadyan@gmail.com>
2022-05-26 17:49:18 +0430

    adding ShippingPage

--- frontent/src/pages/ShippingPage.js
+++ frontent/src/pages/ShippingPage.js
@@ -1,29 +1,49 @@
 import React, {useState} from "react";
 import FormContainer from "../components/FormContainer";
+import {useDispatch, useSelector} from "react-redux";
+import {saveShippingAddress} from "../actions/cartActions";
+import {useNavigate} from "react-router-dom";
 
 
-function ShippingPage(){
-    const [address, setAddress] = useState('')
-    const [city, setCity] = useState('')
-    const [postalCode, setPostalCode] = useState('')
-    const [country, setCountry] = useState('')
+function ShippingPage() {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const cart = useSelector(state => state.cart)
+    const {shippingAddress} = cart
+
+    const [address, setAddress] = useState(shippingAddress.address)
+    const [city, setCity] = useState(shippingAddress.city)
+    const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
+    const [country, setCountry] = useState(shippingAddress.country)
 
     const submitHandler = (e) => {
         e.preventDefault()
-        console.log('submitted')
+        let data = {
+            address: address,
+            city: city,
+            postalCode: postalCode,
+            country: country,
+        }
+        dispatch(saveShippingAddress(data))
+        navigate('/payment/')
     }
     return (
         <FormContainer>
             <h1>Shipping</h1>
             <form onSubmit={submitHandler}>
                 <label htmlFor="address">Address</label>
-                <input type="text" id="address" className="form-control mb-3" value={address} onChange={(e) => setAddress(e.target.value)} placeholder="Enter Address" required/>
+                <input type="text" id="address" className="form-control mb-3" value={address ? address : ''}
+                       onChange={(e) => setAddress(e.target.value)} placeholder="Enter Address" required/>
                 <label htmlFor="city">City</label>
-                <input type="text" id="city" className="form-control mb-3" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Enter City" required/>
+                <input type="text" id="city" className="form-control mb-3" value={city ? city : ''}
+                       onChange={(e) => setCity(e.target.value)} placeholder="Enter City" required/>
                 <label htmlFor="postalCode">PostalCode</label>
-                <input type="text" id="postalCode" className="form-control mb-3" value={postalCode} onChange={(e) => setPostalCode(e.target.value)} placeholder="Enter PostalCode" required/>
+                <input type="text" id="postalCode" className="form-control mb-3" value={postalCode ? postalCode : ''}
+                       onChange={(e) => setPostalCode(e.target.value)} placeholder="Enter PostalCode" required/>
                 <label htmlFor="country">Country</label>
-                <input type="text" id="country" className="form-control mb-3" value={country} onChange={(e) => setCountry(e.target.value)} placeholder="Enter Country" required/>
+                <input type="text" id="country" className="form-control mb-3" value={country ? country : ''}
+                       onChange={(e) => setCountry(e.target.value)} placeholder="Enter Country" required/>
 
                 <input type="submit" value="Continue" className="btn btn-warning"/>
             </form>
